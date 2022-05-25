a23842a17b39952c0fc5a02de26190b91cb57b2a
Ravindra Rathor <ravindra@dyte.io>
2022-05-25 12:28:00 +0530

    feat(skilllynctheme): Added skilllync theme for now

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useCallback, useEffect } from 'react';
 import DyteClient from "@dyte-in/client-core";
 import { DyteMeeting } from "@dyte-in/react-ui-kit";
+import { provideDyteDesignSystem } from '@dyte-in/ui-kit';
 
 const App = () => {
   const [roomName, setRoomName] = useState<string | null>('omgaev-kvcyxj');
@@ -23,7 +24,6 @@ const App = () => {
     if (!res.ok) {
       return;
     }
-
     const authToken = (await res.json()).authToken;
 
     window.meeting = await DyteClient.init({
@@ -35,9 +35,43 @@ const App = () => {
     setActiveMeeting(true);
   };
 
+  handleJoin();
+
+  useEffect(() => {
+    provideDyteDesignSystem(document.body, {
+        colors: {
+            brand: {
+                300: '#00FFE1',
+                400: '#00FFFF',
+                500: '#00E1D4',
+                600: '#007B74',
+                700: '#00655F'
+            },
+            background:{
+                1000: '#FFFFFF',
+                900: '#E6E6E6',
+                800: '#DADADD',
+                700: '#CDCDD0',
+                600: '#C0C0C1'
+            },
+            text: '#071428',
+            "video-bg": "#E5E7EB"
+        }
+    });
+},[]);
+
   if (!activeMeeting) {
+    if(!!handleJoin){
+      return <div style={{
+        width: '100vw',
+        height: '100vh',
+        display:'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>Loading...</div>
+    }
     return (
-      <div className="home">
+      <div className="home" style={{display: 'none'}}>
         <h2>Enter your meeting ID to join</h2>
         <div className="meeting-form">
           <input
@@ -49,9 +83,9 @@ const App = () => {
             <option value="staging">Staging</option>
             <option value="cluster">Prod</option>
           </select>
-          <button type="submit" onClick={() => handleJoin()}>
+          {/* <button type="submit" onClick={() => handleJoin()}>
             Join meeting
-          </button>
+          </button> */}
         </div>
       </div>
     );
