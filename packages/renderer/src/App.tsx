605b6248a2db8d35a5f003a0b1943d915292bcaa
Ravindra Rathor <ravindra@dyte.io>
2022-09-26 23:48:57 +0530

    fix(defaultscreen): added default screen

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -35,49 +35,32 @@ const App = () => {
     setActiveMeeting(true);
   };
 
-  handleJoin();
-
-  useEffect(() => {
-    provideDyteDesignSystem(document.body, {
-      colors: {
-        brand: {
-          300: '#00FFE1',
-          400: '#00FFFF',
-          500: '#00E1D4',
-          600: '#007B74',
-          700: '#00655F',
-        },
-        background: {
-          1000: '#FFFFFF',
-          900: '#E6E6E6',
-          800: '#DADADD',
-          700: '#CDCDD0',
-          600: '#C0C0C1',
-        },
-        text: '#071428',
-        'video-bg': '#E5E7EB',
-      },
-    });
-  }, []);
+  // useEffect(() => {
+  //   provideDyteDesignSystem(document.body, {
+  //     colors: {
+  //       brand: {
+  //         300: '#00FFE1',
+  //         400: '#00FFFF',
+  //         500: '#00E1D4',
+  //         600: '#007B74',
+  //         700: '#00655F',
+  //       },
+  //       background: {
+  //         1000: '#FFFFFF',
+  //         900: '#E6E6E6',
+  //         800: '#DADADD',
+  //         700: '#CDCDD0',
+  //         600: '#C0C0C1',
+  //       },
+  //       text: '#071428',
+  //       'video-bg': '#E5E7EB',
+  //     },
+  //   });
+  // }, []); // skilllync theme
 
   if (!activeMeeting) {
-    if (!!handleJoin) {
-      return (
-        <div
-          style={{
-            width: '100vw',
-            height: '100vh',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          Loading...
-        </div>
-      );
-    }
     return (
-      <div className="home" style={{ display: 'none' }}>
+      <div className="home">
         <h2>Enter your meeting ID to join</h2>
         <div className="meeting-form">
           <input
@@ -89,9 +72,9 @@ const App = () => {
             <option value="staging">Staging</option>
             <option value="cluster">Prod</option>
           </select>
-          {/* <button type="submit" onClick={() => handleJoin()}>
+          <button type="submit" onClick={() => handleJoin()}>
             Join meeting
-          </button> */}
+          </button>
         </div>
       </div>
     );
