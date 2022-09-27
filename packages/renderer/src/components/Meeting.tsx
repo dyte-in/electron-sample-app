b604b13e971fa85f4033e66822f58e4835357ef9
Vaibhav Shinde <vaibhav@dyte.io>
2022-09-27 17:20:54 +0530

    fix: type error

--- packages/renderer/src/components/Meeting.tsx
+++ packages/renderer/src/components/Meeting.tsx
@@ -33,5 +33,5 @@ export default function Meeting({ authToken, roomName, setAction }: Props) {
     })();
   }, []);
 
-  return <DyteMeeting meeting={meeting} showSetupScreen />;
+  return <DyteMeeting meeting={meeting!} showSetupScreen />;
 }
